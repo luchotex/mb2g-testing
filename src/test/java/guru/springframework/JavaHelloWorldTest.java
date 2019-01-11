89424be828c0ca6ecee890e002a492dfde3f818e
luchotex.overactive <lkupferberg@overactive.com>
2019-01-11 18:07:39 -0400

    -- Adding missing files.

--- src/test/java/guru/springframework/JavaHelloWorldTest.java
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -1,5 +1,16 @@
+package guru.springframework;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class JavaHelloWorldTest {
 
+    @Test
+    public void getHello() {
+
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+
+        assertEquals("Hello World", javaHelloWorld.getHello());
+    }
 }
