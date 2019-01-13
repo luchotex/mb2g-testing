9f164d5bb9c1a6cb05b6f5eaf00ace9fddd37dca
Luis Kupferberg <lkupferberg@overactive.com>
2019-01-12 20:00:55 -0400

    -- Add support for the junit 4 and 5

---
+++ src/test/java/guru/springframework/JUnit5FauxTest.java
@@ -0,0 +1,14 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by Luis
+ * on 12/01/2019.
+ */
+public class JUnit5FauxTest {
+    @Test
+    void someTestforJunit5() {
+        System.out.println("I ran...");
+    }
+}
