d3947983fef7264825b581f10ac3116836b19170
luchotex.overactive <lkupferberg@overactive.com>
2019-01-15 14:16:29 -0400

    -- Adding the failsafe plugin and a IT test.

---
+++ src/test/java/guru/springframework/JavaHelloWorldIT.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloWorldIT {
+
+    @Test
+    void myFaultIntegrationTest() {
+
+        System.out.println("My IT Ran");
+    }
+}
